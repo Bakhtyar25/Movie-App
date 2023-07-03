114e6a0b4262cf1cc2bd10a8517b54c029414e1f
bakhtyar25 <bakhtyarmhamad12@gmail.com>
2023-07-03 17:48:51 +0300

    dk

--- src/app/movies/[id].tsx
+++ src/app/movies/[id].tsx
@@ -1,129 +1,129 @@
-// import VoteAverage from "../components/MoviesPage/MoviePage/VoteAverage";
-// import Head from "next/head";
-// import Image from "next/image"
-// import Link from "next/link";
+import VoteAverage from "../components/MoviesPage/MoviePage/VoteAverage";
+import Head from "next/head";
+import Image from "next/image"
+import Link from "next/link";
 
-// const imagePath = "https://image.tmdb.org/t/p/";
+const imagePath = "https://image.tmdb.org/t/p/";
 
-// type genre = {
-//     name:string,
-//     id:number,
-// }
-// interface props{
-//     movie:{
-//         id:string,
-//         title:string,
-//         poster_path:string,
-//         backdrop_path:string,
-//         release_date:string,
-//         runtime:number,
-//         genres:[genre],
-//         overview:string | undefined,
-//         vote_average:number,
-//         tagline:string|undefined
-//     },
-//     width:number|undefined,
-// }
-
-
-// const Movie: React.FC <props> = ({movie,width}) => {
-//     return (
-//         <main className='grow relative min-w-full '>
-//         <Head>
-//             <title>{movie.title}</title>
-//         </Head> 
-//             {movie &&
-//             <>
-//                 <div className="absolute w-full h-full" >
-//                     <Image fill={true} priority={true} blurDataURL={imagePath + "w500" + movie.backdrop_path}
-//                     className="object-cover"
-//                     quality={50} src={imagePath + "original" + movie.backdrop_path} alt="" />
-//                     <div className="absolute  w-full h-full" style={{background: "rgba(61,61,64,0.7)"}}></div>
-//                 </div>
-//                 <div className="max-w-5xl mt-5 z-10 h-fit sm:h-full flex flex-col sm:flex-row items-center mx-auto">
-//                     <div className="relative hover:scale-95 transition-all duration-100" 
-//                         style={{
-//                             width:"200px",
-//                             height:"300px",
-//                             aspectRatio:"2/3",
-//                             alignItems:"center"
-//                         }}>
-//                         <Image className="rounded-md relative" priority={true} src={imagePath + "original" + movie.poster_path} 
-//                         blurDataURL={imagePath + "w300" + movie.backdrop_path}
-//                         quality={30} width={200} height={300} alt="" />
-//                     </div>
-//                     <div className="relative h-fit">
-//                         <div className=" text-2xl sm:text-4xl px-2 text-center sm:text-left">
-//                             <span className="font-bold"><Link href={"/movies/" + movie.id}>{movie.title}</Link></span>  
-//                             <span className=" text-lg sm:text-3xl text-gray-300"> ({movie.release_date.slice(0,4)})</span>
-//                         </div>
-//                         <div className="flex flex-row p-2 bg-slate-300/[0.5]  sm:bg-none">
-//                             <ul className="sm:list-disc flex flex-col sm:flex-row justify-between">
-//                                 <li className="mx-2">
-//                                     <ul className="list-none flex flex-row">
-//                                         {movie.genres.map( (genre) =>
-//                                             <li key={genre.id} className="px-2 font-bold hover:text-blue-300">
-//                                                 <Link href={`/genres/${genre.id}`+`?genre_name=${genre.name.toLowerCase()}`}>
-//                                                     {genre.name}
-//                                                 </Link>
-//                                             </li>
-//                                         )}
-//                                     </ul>
-//                                 </li>
-//                                 <li className="mx-2 ">{ runtime(movie.runtime)}</li>
-//                             </ul>
-//                         </div>
-//                         <div className="relative flex h-[70px] flex-row mx-5 my-2 " style={{alignItems:"center"}}>
-//                             <VoteAverage vote_average={movie.vote_average}/>
-//                             <div className="absolute left-[75px] col-1">
-//                                 <div>User</div>
-//                                 <div>Score</div>
-//                             </div>
-//                         </div>
-//                         <div className="pb-2 mb-4 px-2">
-//                             {movie.tagline !==undefined &&
-//                                 <p className="text-md sm:text-lg font-extralight text-gray-100 py-2"><i>{movie.tagline}</i></p>
-//                             }
-//                             <h2 className="text-lg sm:text-xl mb-2 font-bold">Overview</h2>
-//                             {movie.overview !== undefined &&
-//                             <p className="text-md">{movie.overview}</p>
-//                         }
-//                         </div>
-//                     </div>
-//                 </div>
-//             </>
-//             }
-//         </main>
-//     );
-// }
-
-// type context = {
-//     params:{
-//         id:string;
-//     }
-// }
-
-// export const getServerSideProps = async(context:context) =>{
-//     const apiKey = process.env.API_KEY
-//     const data = await fetch(`https://api.themoviedb.org/3/movie/${context.params.id}?api_key=${process.env.API_KEY}`)
-//     const movie = await data.json()
-//     return {props:{movie}}
-// } 
-
-// export default Movie;
+type genre = {
+    name:string,
+    id:number,
+}
+interface props{
+    movie:{
+        id:string,
+        title:string,
+        poster_path:string,
+        backdrop_path:string,
+        release_date:string,
+        runtime:number,
+        genres:[genre],
+        overview:string | undefined,
+        vote_average:number,
+        tagline:string|undefined
+    },
+    width:number|undefined,
+}
 
 
-// function runtime (movieRuntime:number){
-//     let run = movieRuntime
-//     let result:string = ""
-//     let i = 0
-//     while(run >= 60){
-//         i++
-//         run -= 60
-//     }
-//     if(i>0)
-//         result = i.toString() + "h "
-//     if(run>0)
-//         result = result + run + "min" 
-//     return result
-// }
+const Movie: React.FC <props> = ({movie,width}) => {
+    return (
+        <main className='grow relative min-w-full '>
+        <Head>
+            <title>{movie.title}</title>
+        </Head> 
+            {movie &&
+            <>
+                <div className="absolute w-full h-full" >
+                    <Image fill={true} priority={true} blurDataURL={imagePath + "w500" + movie.backdrop_path}
+                    className="object-cover"
+                    quality={50} src={imagePath + "original" + movie.backdrop_path} alt="" />
+                    <div className="absolute  w-full h-full" style={{background: "rgba(61,61,64,0.7)"}}></div>
+                </div>
+                <div className="max-w-5xl mt-5 z-10 h-fit sm:h-full flex flex-col sm:flex-row items-center mx-auto">
+                    <div className="relative hover:scale-95 transition-all duration-100" 
+                        style={{
+                            width:"200px",
+                            height:"300px",
+                            aspectRatio:"2/3",
+                            alignItems:"center"
+                        }}>
+                        <Image className="rounded-md relative" priority={true} src={imagePath + "original" + movie.poster_path} 
+                        blurDataURL={imagePath + "w300" + movie.backdrop_path}
+                        quality={30} width={200} height={300} alt="" />
+                    </div>
+                    <div className="relative h-fit">
+                        <div className=" text-2xl sm:text-4xl px-2 text-center sm:text-left">
+                            <span className="font-bold"><Link href={"/movies/" + movie.id}>{movie.title}</Link></span>  
+                            <span className=" text-lg sm:text-3xl text-gray-300"> ({movie.release_date.slice(0,4)})</span>
+                        </div>
+                        <div className="flex flex-row p-2 bg-slate-300/[0.5]  sm:bg-none">
+                            <ul className="sm:list-disc flex flex-col sm:flex-row justify-between">
+                                <li className="mx-2">
+                                    <ul className="list-none flex flex-row">
+                                        {movie.genres.map( (genre) =>
+                                            <li key={genre.id} className="px-2 font-bold hover:text-blue-300">
+                                                <Link href={`/genres/${genre.id}`+`?genre_name=${genre.name.toLowerCase()}`}>
+                                                    {genre.name}
+                                                </Link>
+                                            </li>
+                                        )}
+                                    </ul>
+                                </li>
+                                <li className="mx-2 ">{ runtime(movie.runtime)}</li>
+                            </ul>
+                        </div>
+                        <div className="relative flex h-[70px] flex-row mx-5 my-2 " style={{alignItems:"center"}}>
+                            <VoteAverage vote_average={movie.vote_average}/>
+                            <div className="absolute left-[75px] col-1">
+                                <div>User</div>
+                                <div>Score</div>
+                            </div>
+                        </div>
+                        <div className="pb-2 mb-4 px-2">
+                            {movie.tagline !==undefined &&
+                                <p className="text-md sm:text-lg font-extralight text-gray-100 py-2"><i>{movie.tagline}</i></p>
+                            }
+                            <h2 className="text-lg sm:text-xl mb-2 font-bold">Overview</h2>
+                            {movie.overview !== undefined &&
+                            <p className="text-md">{movie.overview}</p>
+                        }
+                        </div>
+                    </div>
+                </div>
+            </>
+            }
+        </main>
+    );
+}
+
+type context = {
+    params:{
+        id:string;
+    }
+}
+
+export const getServerSideProps = async(context:context) =>{
+    const apiKey = process.env.API_KEY
+    const data = await fetch(`https://api.themoviedb.org/3/movie/${context.params.id}?api_key=${process.env.API_KEY}`)
+    const movie = await data.json()
+    return {props:{movie}}
+} 
+
+export default Movie;
+
+
+function runtime (movieRuntime:number){
+    let run = movieRuntime
+    let result:string = ""
+    let i = 0
+    while(run >= 60){
+        i++
+        run -= 60
+    }
+    if(i>0)
+        result = i.toString() + "h "
+    if(run>0)
+        result = result + run + "min" 
+    return result
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,121 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 export default function page({}: Props) {
   return (
-    <div>page</div>
-  )
+    <>
+      <div className="w-[390px] h-[594px] relative bg-neutral-50 rounded-2xl">
+        <div className="w-[390px] h-[50px] left-0 top-[75px] absolute bg-slate-100 border border-emerald-500" />
+        <div className="w-[390px] h-[309px] left-0 top-[125px] absolute">
+          <div className="w-[390px] h-[50px] left-0 top-0 absolute bg-slate-100 border border border-gray-900" />
+          <div className="w-[102px] h-[47px] left-[11px] top-[12px] absolute text-gray-900 text-[20px] font-normal">
+            Home
+          </div>
+          <div className="w-[390px] h-[50px] left-0 top-[50px] absolute bg-slate-100 border-gray-900" />
+          <div className="w-[102px] h-[47px] left-[11px] top-[62px] absolute text-gray-900 text-[20px] font-normal">
+            Pricing{" "}
+          </div>
+          <div className="w-[390px] h-[50px] left-0 top-[100px] absolute bg-slate-100 border border-gray-900" />
+          <div className="w-[102px] h-[47px] left-[11px] top-[112px] absolute text-gray-900 text-[20px] font-normal">
+            Airports{" "}
+          </div>
+          <div className="w-[390px] h-[50px] left-0 top-[150px] absolute bg-slate-100 border border border-gray-900" />
+          <div className="w-[162px] h-[47px] left-[11px] top-[162px] absolute text-gray-900 text-[20px] font-normal capitalize">
+            Reservations
+          </div>
+          <div className="w-[390px] h-[50px] left-0 top-[200px] absolute bg-slate-100 border-gray-900" />
+          <div className="w-[102px] h-[47px] left-[11px] top-[212px] absolute text-gray-900 text-[20px] font-normal">
+            About us
+          </div>
+          <div className="w-[390px] h-[50px] left-0 top-[250px] absolute bg-slate-100 border border border-gray-900" />
+          <div className="w-[140px] h-[47px] left-[11px] top-[262px] absolute text-gray-900 text-[20px] font-normal">
+            Contact us
+          </div>
+          <div className="w-[390px] h-[300px] left-0 top-0 absolute bg-slate-100 bg-opacity-5" />
+        </div>
+        <div className="w-[390px] h-[75px] left-0 top-0 absolute">
+          <div className="w-[390px] h-[75px] left-0 top-0 absolute bg-slate-100" />
+          <div className="w-[50px] h-[50px] left-[25px] top-[14px] absolute bg-zinc-300 rounded-[25px]" />
+          <img
+            className="w-[66px] h-[61px] left-[19px] top-[14px] absolute"
+            src="https://via.placeholder.com/66x61"
+          />
+        </div>
+        <div className="w-[260px] left-[10px] top-[83px] absolute justify-center items-start inline-flex">
+          <div className="grow shrink basis-0 h-36 flex-col justify-center items-center inline-flex">
+            <div className="w-[130px] h-36 relative">
+              <div className="w-[130px] h-9 left-0 top-0 absolute bg-zinc-200 rounded-tl border border border-emerald-500" />
+              <div className="w-[62px] h-[25px] left-[40px] top-[5px] absolute text-black text-[15px] font-normal">
+                Euro
+              </div>
+              <div className="w-[25px] h-[25px] left-[9px] top-[5px] absolute bg-teal-950 rounded-full" />
+              <div className="w-6 h-6 left-[9px] top-[5px] absolute" />
+              <div className="w-[130px] h-9 left-0 top-[36px] absolute">
+                <div className="w-[130px] h-9 left-0 top-0 absolute bg-zinc-200 border border border border-emerald-500" />
+                <div className="w-[25px] h-[25px] left-[9px] top-[6px] absolute bg-teal-950 rounded-full" />
+                <div className="w-6 h-6 left-[9px] top-[6px] absolute" />
+                <div className="w-[62px] h-[25px] left-[40px] top-[6px] absolute text-black text-[15px] font-normal">
+                  Euro
+                </div>
+              </div>
+              <div className="w-[130px] h-9 left-0 top-[108px] absolute">
+                <div className="w-[130px] h-9 left-0 top-0 absolute bg-zinc-200 rounded-bl rounded-br border border border border-emerald-500" />
+                <div className="w-[25px] h-[25px] left-[9px] top-[6px] absolute bg-teal-950 rounded-full" />
+                <div className="w-[62px] h-[25px] left-[40px] top-[6px] absolute text-black text-[15px] font-normal">
+                  Pound
+                </div>
+                <div className="w-[25px] h-6 left-[9px] top-[6px] absolute" />
+              </div>
+              <div className="w-[130px] h-9 left-0 top-[72px] absolute">
+                <div className="w-[130px] h-9 left-0 top-0 absolute bg-zinc-200 border border border border-emerald-500" />
+                <div className="w-[25px] h-[25px] left-[9px] top-[6px] absolute bg-teal-950 rounded-full" />
+                <div className="w-[62px] h-[25px] left-[40px] top-[6px] absolute text-black text-[15px] font-normal">
+                  Dollar
+                </div>
+                <div className="w-[25px] h-[25px] left-[9px] top-[6px] absolute" />
+              </div>
+            </div>
+          </div>
+          <div className="grow shrink basis-0 h-36 flex-col justify-center items-center inline-flex">
+            <div className="w-[130px] h-36 relative">
+              <div className="w-[130px] h-9 left-0 top-0 absolute bg-zinc-200 rounded-tr border border border-emerald-500" />
+              <div className="w-[69px] h-[25px] left-[39px] top-[5px] absolute text-black text-[15px] font-normal">
+                English
+              </div>
+              <div className="w-[130px] h-9 left-0 top-[36px] absolute">
+                <div className="w-[130px] h-9 left-0 top-0 absolute bg-zinc-200 border border border border border-emerald-500" />
+                <div className="w-[90px] h-[25px] left-[40px] top-[6px] absolute text-black text-[15px] font-normal">
+                  Netherlands
+                </div>
+                <div className="w-[25px] h-[25px] left-[9px] top-[6px] absolute">
+                  <div className="w-[25px] h-[25px] left-0 top-0 absolute bg-zinc-300 rounded-full" />
+                  <div className="w-[8.33px] h-[25px] left-0 top-[25px] absolute origin-top-left -rotate-90 bg-sky-800" />
+                  <div className="w-[8.33px] h-[25px] left-0 top-[16.67px] absolute origin-top-left -rotate-90 bg-neutral-100" />
+                  <div className="w-[8.33px] h-[25px] left-0 top-[8.33px] absolute origin-top-left -rotate-90 bg-red-800" />
+                </div>
+              </div>
+              <div className="w-[130px] h-9 left-0 top-[108px] absolute">
+                <div className="w-[130px] h-9 left-0 top-0 absolute bg-zinc-200 rounded-bl rounded-br border border border border border-emerald-500" />
+                <div className="w-[25px] h-[25px] left-[9px] top-[6px] absolute bg-teal-950 rounded-full" />
+                <div className="w-[62px] h-[25px] left-[40px] top-[6px] absolute text-black text-[15px] font-normal">
+                  Arabic
+                </div>
+                <div className="w-[25px] h-[25px] left-[9px] top-[6px] absolute">
+                  <div className="w-[25px] h-[25px] left-0 top-0 absolute bg-zinc-300 rounded-full" />
+                  <div className="w-[25px] h-[25px] left-0 top-0 absolute bg-emerald-900" />
+                </div>
+              </div>
+              <div className="w-[130px] h-9 left-0 top-[72px] absolute">
+                <div className="w-[130px] h-9 left-0 top-0 absolute bg-zinc-200 border border border border border-emerald-500" />
+                <div className="w-[62px] h-[25px] left-[40px] top-[6px] absolute text-black text-[15px] font-normal">
+                  English
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
